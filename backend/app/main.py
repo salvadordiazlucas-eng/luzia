ede50f80637fb5c1a2cd1aec3f8aa46592ed7206
salvadordiazlucas-eng <salvadordiazlucas@gmail.com>
2026-03-12 22:53:54 +0100

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 
-from .database import engine, Base, get_db
+from .database import engine, get_db, Base
 from . import models
 
-# Crear tablas automáticamente si no existen
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
@@ -12,6 +12,7 @@ app = FastAPI(
     description="Sistema inteligente de gestión de reservas para restaurantes",
     version="1.0.0"
 )
+
 
 @app.get("/")
 def root():
@@ -21,9 +22,31 @@ def root():
 @app.post("/restaurants")
 def create_restaurant(name: str, db: Session = Depends(get_db)):
     restaurant = models.Restaurant(name=name)
-
     db.add(restaurant)
     db.commit()
     db.refresh(restaurant)
-
     return restaurant
+
+
+@app.post("/locations")
+def create_location(
+    restaurant_id: int,
+    name: str,
+    address: str,
+    city: str,
+    capacity: int,
+    db: Session = Depends(get_db)
+):
+    location = models.Location(
+        restaurant_id=restaurant_id,
+        name=name,
+        address=address,
+        city=city,
+        capacity=capacity
+    )
+
+    db.add(location)
+    db.commit()
+    db.refresh(location)
+
+    return location
